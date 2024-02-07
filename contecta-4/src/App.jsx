9a95ec4b3e02095a9d02ab603f82a4378ca037e9
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-02-06 23:32:49 -0400

    Creando la funcion handleClick para que al hacer click, se coloque una nueva ficha en el tablero de conecta 4

--- contecta-4/src/App.jsx
+++ contecta-4/src/App.jsx
@@ -10,16 +10,41 @@ function App() {
     ["", "", "", "", "", "", ""],
   ]);
 
+  const [turn, setTurn] = useState("1");
+
+  const handleClick = (col) => {
+    console.log(col);
+    const newBoard = [...board];
+
+    for (let i = 5; i >= 0; i--) {
+      if (newBoard[i][col] === "") {
+        newBoard[i][col] = turn;
+        setTurn(turn === "1" ? "2" : "1");
+        setBoard(newBoard);
+        return;
+      }
+    }
+
+    alert("Column is full");
+  };
+
   return (
     <>
       <div className="container">
         <div className="board">
           {board.map((row, rowIndex) =>
             row.map((cell, cellIndex) => (
-              <div
-                key={`${rowIndex}-${cellIndex}`}
-                className="cell" >
-                <div className={ cell === "1" ? "circle-red circle" : cell === "2" ? "circle-yellow circle" : "circle"}></div>
+              <div key={`${rowIndex}-${cellIndex}`} className="cell">
+                <div
+                  className={
+                    cell === "1"
+                      ? "circle-red circle"
+                      : cell === "2"
+                      ? "circle-yellow circle"
+                      : "circle"
+                  }
+                  onClick={() => handleClick(cellIndex)}
+                ></div>
               </div>
             ))
           )}
