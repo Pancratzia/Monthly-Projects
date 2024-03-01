c1c2ab60ab60ebabef24327c7d1fc8f3e986e618
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-02-29 23:53:20 -0400

    Terminando los estilos del modal

--- starwars-wiki/src/App.jsx
+++ starwars-wiki/src/App.jsx
@@ -95,6 +95,16 @@ export const Info = ({ copy = false }) => {
 };
 
 export const Modal = ({ character, closeModal }) => {
+  const {
+    name,
+    height,
+    mass,
+    hair_color,
+    skin_color,
+    eye_color,
+    birth_year,
+    gender,
+  } = character;
   return (
     <div className="modal">
       <div className="modal-content">
@@ -103,8 +113,19 @@ export const Modal = ({ character, closeModal }) => {
             &times;
           </button>
         </div>
-        <h2>{character.name}</h2>
-        <p>Gender: {character.gender}</p>
+        <div className="modal-body">
+          <h2 className="modal-title">{name}</h2>
+          <h4 className="modal-subtitle">Birth Year: {birth_year}</h4>
+
+          <ul className="modal-main-info">
+            <li><span>Gender:</span> {gender.toUpperCase()}</li>
+            <li><span>Height:</span> {height} cm</li>
+            <li><span>Mass:</span> {mass} kg</li>
+            <li><span>Hair Color:</span> {hair_color}</li>
+            <li><span>Skin Color:</span> {skin_color}</li>
+            <li><span>Eye Color:</span> {eye_color}</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
