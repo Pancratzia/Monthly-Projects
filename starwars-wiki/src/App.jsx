6a3316c6731f22962ee9a148b1617ff2974f2b73
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-01 00:06:53 -0400

    Creando el hook useFetch

--- starwars-wiki/src/App.jsx
+++ starwars-wiki/src/App.jsx
@@ -1,39 +1,9 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Background } from "./components/Background/Background";
 import { Info } from "./components/Info/Info";
 import { Modal } from "./components/Modal/Modal";
-import axios from "axios";
-
-function useFetch(url, multipleUrl = false) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  const fetchData = useCallback(async (fetchUrl) => {
-    try {
-      const response = await axios.get(fetchUrl);
-      const responseData = response.data;
-      setData((prevData) => ({ ...prevData, ...responseData }));
-      if (responseData.next !== null && multipleUrl) {
-        await fetchData(responseData.next);
-      } else {
-        setLoading(false);
-      }
-    } catch (err) {
-      setError(err);
-      setLoading(false);
-    }
-  }, [multipleUrl]);
-
-  useEffect(() => {
-    if (!data) {
-      fetchData(url);
-    }
-  }, [url, data, fetchData]);
-
-  return { data, loading, error };
-}
+import { useFetch } from "./hooks/useFetch";
 
 function App() {
   const { data, loading, error } = useFetch(
---
+++ starwars-wiki/src/hooks/useFetch.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState, useCallback } from "react";
+import axios from "axios";
+
+function useFetch(url, multipleUrl = false) {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+  
+    const fetchData = useCallback(async (fetchUrl) => {
+      try {
+        const response = await axios.get(fetchUrl);
+        const responseData = response.data;
+        setData((prevData) => ({ ...prevData, ...responseData }));
+        if (responseData.next !== null && multipleUrl) {
+          await fetchData(responseData.next);
+        } else {
+          setLoading(false);
+        }
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    }, [multipleUrl]);
+  
+    useEffect(() => {
+      if (!data) {
+        fetchData(url);
+      }
+    }, [url, data, fetchData]);
+  
+    return { data, loading, error };
+  }
+
+  export default useFetch;
+  
