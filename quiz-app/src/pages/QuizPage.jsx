efd792667b56eb82a0b65707c77d83b96934d737
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-11 22:59:48 -0400

    Se van mostrando las preguntas una por una, y de ser correcta se va sumando al score

--- quiz-app/src/pages/QuizPage.jsx
+++ quiz-app/src/pages/QuizPage.jsx
@@ -9,6 +9,8 @@ const QuizPage = () => {
   }
 
   const [selectedQuestions, setSelectedQuestions] = useState([]);
+  const [actualQuestion, setActualQuestion] = useState(0);
+  const [score, setScore] = useState(0);
 
   const getQuestions = async () => {
     try {
@@ -27,6 +29,7 @@ const QuizPage = () => {
       });
 
       setSelectedQuestions(getNRandomQuestions(q, 10));
+      setActualQuestion(0);
     } catch (error) {
       console.error("Error getting questions: ", error);
     }
@@ -36,16 +39,46 @@ const QuizPage = () => {
     const shuffled = questions.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, n);
   };
+  
+  const handleSelectedAnswer = (correcta) => {
+
+    if (correcta) {
+      setScore(score + 1);
+    }
+
+    setActualQuestion(actualQuestion + 1);
+  }
 
   useEffect(() => {
     getQuestions();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  console.log(selectedQuestions);
+  return (
+    <div className="quiz">
+      <h1>Answer As Fast As You Can!</h1>
+      <h3>Actual Score: {score}</h3>
 
-  return <div>QuizPage</div>;
+      {actualQuestion < selectedQuestions.length && (
+        <div>
+          <h2>{selectedQuestions[actualQuestion].pregunta}</h2>
+
+          <ul className="options">
+
+            {selectedQuestions[actualQuestion].respuestas.map(
+              (option, index) => (
+                <li key={index}>
+                  <button onClick={() => handleSelectedAnswer(option.correcta)}>
+                    {option.texto}
+                  </button>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default QuizPage;
-
