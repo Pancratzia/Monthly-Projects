60d2509f8ebd317c10004550c3b715142533993a
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-13 12:10:15 -0400

    Integrando un header y un footer

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,10 +1,14 @@
 import AppRoutes from "./AppRoutes"
+import Header from "./components/layouts/Header"
+import Footer from "./components/layouts/Footer"
 
 function App() {
 
   return (
     <>
+      <Header />
       <AppRoutes />
+      <Footer />
     </>
   )
 }
