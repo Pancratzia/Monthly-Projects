e75e3b516b4cbdc4724653362fa05ce1a55e196e
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-14 12:30:28 -0400

    Dando estilos al titulo y al borde del container

--- quiz-app/src/pages/UserPage.jsx
+++ quiz-app/src/pages/UserPage.jsx
@@ -1,17 +1,30 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useUsers } from "../hooks/useUsers";
+import '../assets/styles/userPage.css'
 
 const UserPage = () => {
   const { handleSubmit } = useUsers();
 
   return (
-    <div>
+    <div className="userPage container">
+      <h2 className="title">Hi! Do you want to test your knowledge?</h2>
+      <h3 className="subtitle">Instructions:</h3>
+
+      <ol className="instructions">
+        <li>Enter an original nickname.</li>
+        <li>Press "Submit".</li>
+        <li>There willl be 10 questions, each one with three possible answers, but only one will be correct.</li>
+        <li>You'll have 30 seconds to answer each question. If you don't answer in time, the question will be skipped.</li>
+        <li>If you answer correctly, you'll as many points as time left in seconds.</li>
+      </ol>
+
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name: </label>
+        <label htmlFor="name">Nickname: </label>
         <input
           type="text"
           name="name"
           id="name"
-          placeholder="Enter your name"
+          placeholder="Enter your nickname"
 
         />
         <input type="submit" value="Submit" />
