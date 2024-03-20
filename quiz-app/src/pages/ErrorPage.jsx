8017c229eb563c2e3581f94a46c7f23fcb0b1b00
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-20 12:46:50 -0400

    Usando las traducciones en la página de error

--- quiz-app/src/pages/ErrorPage.jsx
+++ quiz-app/src/pages/ErrorPage.jsx
@@ -1,10 +1,12 @@
+import { useTranslation } from 'react-i18next'
 import '../assets/styles/errorPage.css'
 
 const ErrorPage = () => {
+  const {t} = useTranslation('global');
   return (
     <div className="errorPage container">
-      <h1 className="title">Error 404</h1>
-      <p className="subtitle">Page not found</p>
+      <h1 className="title">{t('ErrorPage.title')}</h1>
+      <p className="subtitle">{t('ErrorPage.text')}</p>
     </div>
   )
 }
