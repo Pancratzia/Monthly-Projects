e32fda481aef53f4745a0e67f92b9be122c555a5
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-04-16 13:07:45 -0400

    Creando el Container personalizado

--- pomodoro/src/App.jsx
+++ pomodoro/src/App.jsx
@@ -4,7 +4,6 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   )
 }
--- pomodoro/tailwind.config.js
+++ pomodoro/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container: false,
+    extend: {
+      
+    },
+  },
+  corePlugins: {
+    container: false,
   },
   plugins: [],
 }
