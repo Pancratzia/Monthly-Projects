184e8c5db291496ed732d1c2b1c35e4ecef7ad97
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-04-17 14:09:18 -0400

    creando  un contenedor en forma de telefono para el pomodoro

--- pomodoro/src/App.jsx
+++ pomodoro/src/App.jsx
@@ -1,11 +1,16 @@
-
-
 function App() {
-
   return (
     <>
+      <div className="min-h-screen w-screen bg-purple-950 flex justify-center items-center py-2">
+
+        <div id="phone" className="max-w-[360px] max-h-[722px] relative">
+
+          <div className="relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[87%] h-[95%] bg-white rounded-[45px]"></div>
+        </div>
+
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
