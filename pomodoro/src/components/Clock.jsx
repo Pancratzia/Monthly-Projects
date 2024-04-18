2b6cf3feeeb8d2f02f27a4c61a0feedee8554726
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-04-18 19:09:03 -0400

    Se crea un estilo de gradiente conico para el reloj

--- pomodoro/src/components/Clock.jsx
+++ pomodoro/src/components/Clock.jsx
@@ -1,11 +1,19 @@
+import PropTypes from "prop-types";
 
+const Clock = ({ progress = "0" }) => {
 
-const Clock = () => {
+    
   return (
-    
-        <h1>Clock</h1>
-    
-  )
-}
+    <div className="flex flex-col justify-center items-center  rounded-full w-44 h-44 drop-shadow-2xl conic-gradient-progress" style={{ "--progress": `${progress}%` }}>
+      <div className="flex flex-col justify-center items-center rounded-full w-40 h-40 bg-purple-200">
+        <h3 className="text-3xl font-bold text-purple-950">25:00</h3>
+      </div>
+    </div>
+  );
+};
 
-export default Clock
+export default Clock;
+
+Clock.propTypes = {
+  progress: PropTypes.number,
+}
