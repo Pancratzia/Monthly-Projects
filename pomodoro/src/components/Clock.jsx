9dcba04bf3798be723e0a31cee056f60fad82b72
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-04-21 14:05:21 -0400

    Finalizando el progres-bard utilizando otra dependencia actualizada más recientemente

--- pomodoro/src/components/Clock.jsx
+++ pomodoro/src/components/Clock.jsx
@@ -1,30 +1,23 @@
 import PropTypes from "prop-types";
-import {
-  CircularProgressbarWithChildren,
-  buildStyles,
-} from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
+import { CircularProgressBar } from "react-percentage-bar";
+import "react-percentage-bar/dist/index.css";
 
-const Clock = ({
-  percentage = 0,
-  activity = "Trabajando",
-  time = "25:00",
-}) => {
+const Clock = ({ percentage = 0, activity = "Trabajando", time = "25:00" }) => {
   return (
     <div className="w-48 h-48">
-      <CircularProgressbarWithChildren
-        value={percentage}
-        styles={buildStyles({
-          textColor: "rgb(59 7 100)",
-          pathColor: "rgb(59 7 100)",
-          trailColor: "transparent",
-        })}
+      <CircularProgressBar
+        radius={"6.5rem"}
+        roundLineCap={false}
+        percentage={percentage}
+        showPercentage={false}
+        color="#3b0764"
+        shadow="true"
       >
         <div className="w-full h-full flex justify-center items-center flex-col">
           <p className="text-3xl text-purple-950">{time}</p>
           <p className="text-sm text-purple-950">{activity}</p>
         </div>
-      </CircularProgressbarWithChildren>
+      </CircularProgressBar>
     </div>
   );
 };
